8bfa65f82bf9b758e989ae44ad14c15258629f7a
shridhar.k <sk.shridhar.46@gmail.com>
2025-04-17 20:33:16 +0530

    admin-panel

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -3,20 +3,30 @@ import { useForm } from "react-hook-form";
 
 function Admin() {
   const [name, setName] = useState("");
-  const [sub,setSub]=useState("");
+  const [sub, setSub] = useState("");
+  const [url, setUrl] = useState("https://thumbs.dreamstime.com/b/eyeglasses-bearded-asian-man-relaxed-standing-d-vector-avatar-illustration-cheerful-mature-male-cartoon-character-face-confident-297508057.jpg");
+
   const { register, handleSubmit, reset } = useForm();
+
+  const handleSubmitform = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="bg-blue-500 h-auto w-[100vw] p-[1%]">
+
+      
       <div className="">
         <h1 className="text-2xl text-white pb-[2%]">Add Faculty to database</h1>
         <div className="flex">
           <form
             className="h-[50vh] w-[50vw] flex flex-col gap-5 pl-[5%]"
-            action=""
+            onSubmit={handleSubmit(handleSubmitform)}
           >
             <div className="w-[100%]  flex items-center gap-5">
               <span className="text-white"> Faculty Name : </span>
               <input
+                {...register("name")}
                 onChange={(e) => {
                   setName(e.currentTarget.value);
                 }}
@@ -27,7 +37,10 @@ function Admin() {
             </div>
             <div className="w-[100%]  flex items-center gap-5">
               <span className="text-white pl-[1%]"> Department : </span>
-              <select className="bg-white w-[20vw] outline-none rounded-full pl-[15%] h-[5vh]">
+              <select
+                {...register("department")}
+                className="bg-white w-[20vw] outline-none rounded-full pl-[15%] h-[5vh]"
+              >
                 <option value="CSE">CSE</option>
                 <option value="AI&ML">AI&ML</option>
                 <option value="ISE">ISE</option>
@@ -39,7 +52,10 @@ function Admin() {
             </div>
             <div className="w-[100%]  flex items-center gap-5">
               <span className="text-white pl-[3.5%]"> Semester : </span>
-              <select className="bg-white w-[20vw] outline-none rounded-full pl-[17%] h-[5vh]">
+              <select
+                {...register("semester")}
+                className="bg-white w-[20vw] outline-none rounded-full pl-[17%] h-[5vh]"
+              >
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -52,7 +68,9 @@ function Admin() {
             </div>
             <div className="w-[100%]  flex items-center gap-5">
               <span className="text-white"> Subject Code : </span>
-              <input onChange={(e) => {
+              <input
+                {...register("subject")}
+                onChange={(e) => {
                   setSub(e.currentTarget.value);
                 }}
                 className="bg-white h-[5vh] w-[20vw] rounded-full pl-[2%] outline-none"
@@ -63,6 +81,10 @@ function Admin() {
             <div className="w-[100%]  flex items-center gap-5">
               <span className="text-white pl-[3%]"> Profile Pic : </span>
               <input
+                {...register("img")}
+                onChange={(e) => {
+                  setUrl(e.currentTarget.value);
+                }}
                 className="bg-white h-[5vh] w-[20vw] rounded-full pl-[2%] outline-none"
                 type="text"
                 placeholder="URL"
@@ -75,7 +97,9 @@ function Admin() {
           <div className="view  h-[50vh] w-[50vw] pl-[10%]">
             <h1 className="text-white">PROFILE :</h1>
             <div className="bg-[white] w-[20vw] flex flex-col items-center gap-1 rounded-[1cqw]">
-              <div className="bg-blue-200 h-[25vh] w-[25vh] rounded-full mt-[3vh] mb-[2vh] outline"><img src="" alt="" /></div>
+              <div className="bg-blue-200 h-[25vh] w-[25vh] rounded-full mt-[3vh] mb-[2vh] outline overflow-hidden">
+                <img className="object-cover" src={url} alt="" />
+              </div>
               <h1 className="text-xl">Name:{name}</h1>
               <h1 className="text-[#11111170] pb-[5vh]">Subject:{sub}</h1>
             </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,8 +5,8 @@ function Nav() {
   return (
     <div className='h-[10vh] w-full bg-blue-200 flex pl-[2%] pr-[2%] justify-between items-center'>
       <h1 className='text-white font-bold text-xl'>FeedBack</h1>
-      <NavLink className='text-white  text-xl' style={(e)=>{return{color:e.isActive?"green":""}}} to={'/'}>Home</NavLink>
-        <NavLink className='text-white text-xl' style={(e)=>{return{color:e.isActive?"green":""}}} to={'/admin'}>Admin</NavLink>
+      <NavLink className='text-white  text-xl' style={(e)=>{return{color:e.isActive?"blue":""}}} to={'/'}>Home</NavLink>
+        <NavLink className='text-white text-xl' style={(e)=>{return{color:e.isActive?"blue":""}}} to={'/admin'}>Admin</NavLink>
       <input className='bg-white h-[50%] w-[25%] rounded pl-[2%] outline-none' type="text" placeholder='Search' />
     </div>
   )
