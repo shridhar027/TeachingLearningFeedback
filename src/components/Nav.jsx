096f7218373e843e6775ea1935ebfdd57b1b4f98
shridhar.k <sk.shridhar.46@gmail.com>
2025-04-17 11:50:12 +0530

    nav-bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,9 +3,11 @@ import { NavLink } from 'react-router'
 
 function Nav() {
   return (
-    <div>
-        <NavLink style={(e)=>{return{color:e.isActive?"red":""}}} to={'/'}>Home</NavLink>
-        <NavLink style={(e)=>{return{color:e.isActive?"red":""}}} to={'/admin'}>Admin</NavLink>
+    <div className='h-[10vh] w-full bg-red-200 flex pl-[2%] pr-[2%] justify-between items-center'>
+      <h1 className='text-white font-bold text-xl'>FeedBack</h1>
+      <NavLink className='text-white  text-xl' style={(e)=>{return{color:e.isActive?"green":""}}} to={'/'}>Home</NavLink>
+        <NavLink className='text-white text-xl' style={(e)=>{return{color:e.isActive?"green":""}}} to={'/admin'}>Admin</NavLink>
+      <input className='bg-white h-[50%] w-[25%] rounded pl-[2%] outline-none' type="text" placeholder='Search' />
     </div>
   )
 }
